7b810960923557aadd15fddd8d788586859de420
JDBS2021 <79283355+JDBS2021@users.noreply.github.com>
2021-02-22 18:21:55 -0500

    Add files via upload
    EJERCICIO 2 FRAMEWORK

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace EJERCICIO2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Char Sexo;
+            String NombrePersona;
+            int Edad = 0;
+            int PulsacionesHombre;
+            int PulsacionesMujeres;
+
+            Console.Write("digite el nombre de la persona:  ");
+            NombrePersona = (Console.ReadLine());
+            Console.Write("digite la edad de la persona:  ");
+            Edad = Convert.ToInt32(Console.ReadLine());
+            Console.Write("digite el sexo de la persona: (F O M)  ");
+            Sexo = Convert.ToChar(Console.ReadLine());
+
+            PulsacionesHombre = (210 - Edad) / 10;
+            PulsacionesMujeres = (220 - Edad) / 10;
+
+
+            Console.WriteLine("EL NOMBRE DE LA PERSONA  ES: " + NombrePersona);
+            Console.WriteLine("LA EDAD DE LA PERSONA  ES: " + Edad);
+
+            if (Sexo == 'F')
+            {
+                Console.Write("LAS PULSACIONES DE UNA MUJER CADA 10 SEGUNDOS ES: " + PulsacionesMujeres+"BMP"); 
+
+            }
+            if(Sexo == 'M')
+            {
+                Console.Write("LAS PULSACIONES DE UN HOMBRE CADA 10 SEGUNDOS ES: " + PulsacionesHombre+"BMP");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
